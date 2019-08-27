c759a22c74c55c4d2514f90b6e0446e18241a5aa
Вишняк Олександр <bebebe12@ukr.net>
2019-08-27 14:25:22 +0300

    Changed token

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,8 +1,8 @@
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const SERVER_PORT = 3000;
 const app = express();
@@ -13,32 +13,32 @@ app.use(bodyParser.json());
 /////////////////////////////////////////////////////////////////////////
 
 // A token that Facebook will echo back to you as part of callback URL verification.
-const VERIFY_TOKEN = 'YOUR_SECURE_VERIFY_TOKEN';
+const VERIFY_TOKEN = "abc123";
 
 // Endpoint for verifying webhook subscripton
-app.get('/leadgen', (req, res) => {
-  if (!req.query) {
-    res.send({success: false, reason: 'Empty request params'});
-    return;
-  }
-  // Extract a verify token we set in the webhook subscription and a challenge to echo back.
-  const verifyToken = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
+app.get("/leadgen", (req, res) => {
+    if (!req.query) {
+        res.send({ success: false, reason: "Empty request params" });
+        return;
+    }
+    // Extract a verify token we set in the webhook subscription and a challenge to echo back.
+    const verifyToken = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
 
-  if (!verifyToken || !challenge) {
-    res.send({
-      success: false,
-      reason: 'Missing hub.verify_token and hub.challenge params',
-    });
-    return;
-  }
+    if (!verifyToken || !challenge) {
+        res.send({
+            success: false,
+            reason: "Missing hub.verify_token and hub.challenge params"
+        });
+        return;
+    }
 
-  if (verifyToken !== VERIFY_TOKEN) {
-    res.send({success: false, reason: 'Verify token does not match'});
-    return;
-  }
-  // We echo the received challenge back to Facebook to finish the verification process.
-  res.send(challenge);
+    if (verifyToken !== VERIFY_TOKEN) {
+        res.send({ success: false, reason: "Verify token does not match" });
+        return;
+    }
+    // We echo the received challenge back to Facebook to finish the verification process.
+    res.send(challenge);
 });
 
 /////////////////////////////////////////////////////////////////////////
@@ -46,63 +46,63 @@ app.get('/leadgen', (req, res) => {
 /////////////////////////////////////////////////////////////////////////
 
 // Graph API endpoint
-const GRAPH_API_VERSION = 'v2.12';
+const GRAPH_API_VERSION = "v4.0";
 const GRAPH_API_ENDPOINT = `https://graph.facebook.com/${GRAPH_API_VERSION}`;
 // Your system user access token file path.
 // Note: Your system user needs to be an admin of the subscribed page.
 const ACCESS_TOKEN_PATH = path.join(
-  __dirname,
-  '..',
-  'token',
-  'access_token.txt'
+    __dirname,
+    "..",
+    "token",
+    "access_token.txt"
 );
 
 // Read access token for calling graph API.
 if (!fs.existsSync(ACCESS_TOKEN_PATH)) {
-  console.log(`The file ${ACCESS_TOKEN_PATH} does not exist.`);
-  process.exit(1);
+    console.log(`The file ${ACCESS_TOKEN_PATH} does not exist.`);
+    process.exit(1);
 }
-const accessToken = fs.readFileSync(ACCESS_TOKEN_PATH, 'utf-8');
+const accessToken = fs.readFileSync(ACCESS_TOKEN_PATH, "utf-8");
 // Facebook will post realtime leads to this endpoint if we've already subscribed to the webhook in part 1.
-app.post('/leadgen', (req, res) => {
-  const entry = req.body.entry;
+app.post("/leadgen", (req, res) => {
+    const entry = req.body.entry;
 
-  entry.forEach(page => {
-    page.changes.forEach(change => {
-      // We get page, form, and lead IDs from the change here.
-      // We need the lead gen ID to get the lead data.
-      // The form ID and page ID are optional. You may want to record them into your CRM system.
-      const {page_id, form_id, leadgen_id} = change.value;
-      console.log(
-        `Page ID ${page_id}, Form ID ${form_id}, Lead gen ID ${leadgen_id}`
-      );
+    entry.forEach(page => {
+        page.changes.forEach(change => {
+            // We get page, form, and lead IDs from the change here.
+            // We need the lead gen ID to get the lead data.
+            // The form ID and page ID are optional. You may want to record them into your CRM system.
+            const { page_id, form_id, leadgen_id } = change.value;
+            console.log(
+                `Page ID ${page_id}, Form ID ${form_id}, Lead gen ID ${leadgen_id}`
+            );
 
-      // Call graph API to request lead info with the lead ID and access token.
-      const leadgenURI = `${GRAPH_API_ENDPOINT}/${leadgen_id}?access_token=${accessToken}`;
+            // Call graph API to request lead info with the lead ID and access token.
+            const leadgenURI = `${GRAPH_API_ENDPOINT}/${leadgen_id}?access_token=${accessToken}`;
 
-      axios(leadgenURI)
-        .then(response => {
-          const {created_time, id, field_data} = response.data;
+            axios(leadgenURI)
+                .then(response => {
+                    const { created_time, id, field_data } = response.data;
 
-          // Handle lead answer here (insert data into your CRM system)
-          console.log('Lead id', id);
-          console.log('Created time', created_time);
-          field_data.forEach(field => {
-            console.log('Question ', field.name);
-            console.log('Answers ', field.values);
-          });
-        })
-        .catch(error => {
-          // Handle error here
-          console.log(error);
+                    // Handle lead answer here (insert data into your CRM system)
+                    console.log("Lead id", id);
+                    console.log("Created time", created_time);
+                    field_data.forEach(field => {
+                        console.log("Question ", field.name);
+                        console.log("Answers ", field.values);
+                    });
+                })
+                .catch(error => {
+                    // Handle error here
+                    console.log(error);
+                });
         });
     });
-  });
-  // Send HTTP 200 OK status to indicate we've received the update.
-  res.sendStatus(200);
+    // Send HTTP 200 OK status to indicate we've received the update.
+    res.sendStatus(200);
 });
 
 // Start web server
 app.listen(SERVER_PORT, () =>
-  console.log(`Server is listening at localhost:${SERVER_PORT}`)
+    console.log(`Server is listening at localhost:${SERVER_PORT}`)
 );
